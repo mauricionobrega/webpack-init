3910e7cabc8b04028fb06a1fecb86cf9e0dbd678
Mauricio Nobrega - Tibone <mauricio.nobrega@walmart.com>
2017-07-06 08:37:49 -0300

    Webpack init

--- config/webpack/dev.js
+++ config/webpack/dev.js
@@ -2,15 +2,17 @@ const path = require('path'),
   CleanWebpackPlugin = require('clean-webpack-plugin'),
   CopyWebpackPlugin = require('copy-webpack-plugin'),
   ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  _files = require('./_files'),
   _javascripts = require('./_javascripts'),
   _styles = require('./_styles'),
-  utils = require('./utils');
+  utils = require('./utils'),
+  PWD = process.env.PWD;
 
 module.exports = {
   devtool: 'source-map',
   entry: utils.merge(_javascripts, _styles),
   output: {
-    path: path.resolve(process.env.PWD, 'dist'),
+    path: path.resolve(PWD, 'dist'),
     filename: 'js/[chunkHash].[name].js'
   },
   module: {
@@ -30,7 +32,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
-      root: path.resolve(process.env.PWD),
+      root: path.resolve(PWD),
       exclude: ['**/*'],
       verbose: true,
       dry: false
@@ -39,6 +41,14 @@ module.exports = {
       filename: 'styles/[chunkHash].[name].css',
       allChunks: true,
       disable: process.env.NODE_ENV === 'development'
+    }),
+    new CopyWebpackPlugin([], {
+      ignore: [
+        '*.txt', // Doesn't copy any files with a txt extension
+        '**/*', // Doesn't copy any file, even if they start with a dot
+        { glob: '**/*', dot: false } // Doesn't copy any file, except if they start with a dot
+      ],
+      copyUnmodified: true // By default, we only copy modified files during a watch or webpack-dev-server build. Setting this to `true` copies all files.
     })
   ]
 };
